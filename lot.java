c373f6684b9c4b21431935e520b243e43db85994
jatin-x-pal <jatinpaldelhi@gmail.com>
2025-12-19 14:34:30 +0530

    Add files via upload

---
+++ lot.java
@@ -0,0 +1,14 @@
+public class lot {
+    public static void main(String[] args) {
+
+        System.out.println("IoT Device Monitoring Web Application");
+
+        String deviceName = "Temperaturs Sensor";
+        int temperature = 28;
+        boolean deviceStatus = true;
+
+        System.out.println("Device Name: " + deviceName);
+        System.out.println("Temperature: " + temperature + "°C");
+        System.out.println("Device Active: " + deviceStatus);
+    }
+}
